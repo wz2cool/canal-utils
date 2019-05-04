e920a1b4459eeaf83b4ee3849bc9338a73bb3051
wz2cool <wz2cool@live.cn>
2019-05-04 12:14:45 +0800

    add postgresql

--- src/main/java/com/github/wz2cool/canal/utils/converter/postgresql/PostgresqlValuePlaceholderConverter.java
+++ src/main/java/com/github/wz2cool/canal/utils/converter/postgresql/PostgresqlValuePlaceholderConverter.java
@@ -4,9 +4,29 @@ import com.github.wz2cool.canal.utils.converter.IValuePlaceholderConverter;
 import com.github.wz2cool.canal.utils.model.MysqlDataType;
 import com.github.wz2cool.canal.utils.model.ValuePlaceholder;
 
+import java.nio.charset.StandardCharsets;
+
 public class PostgresqlValuePlaceholderConverter implements IValuePlaceholderConverter {
     @Override
     public ValuePlaceholder convert(MysqlDataType mysqlDataType, String value) {
-        return null;
+        ValuePlaceholder result = new ValuePlaceholder();
+        switch (mysqlDataType) {
+            case BIT:
+                result.setPlaceholder("?");
+                result.setValue(value == null ? null : Integer.parseInt(value));
+                break;
+            case TINYBLOB:
+            case BLOB:
+            case MEDIUMBLOB:
+            case LONGBLOB:
+                result.setPlaceholder("?");
+                result.setValue(value == null ? null : value.getBytes(StandardCharsets.ISO_8859_1));
+                break;
+            default:
+                result.setPlaceholder("?");
+                result.setValue(value);
+                break;
+        }
+        return result;
     }
 }
