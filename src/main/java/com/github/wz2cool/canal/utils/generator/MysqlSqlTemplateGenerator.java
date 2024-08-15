6efb38ea5cd2ab81a0859d8e10a7ffe34ae5beb0
RocSea <penghai.yin@innodealing.com>
2024-08-15 12:36:20 +0800

    代码优化

--- src/main/java/com/github/wz2cool/canal/utils/generator/MysqlSqlTemplateGenerator.java
+++ src/main/java/com/github/wz2cool/canal/utils/generator/MysqlSqlTemplateGenerator.java
@@ -3,7 +3,7 @@ package com.github.wz2cool.canal.utils.generator;
 import com.github.wz2cool.canal.utils.converter.BaseAlterSqlConverter;
 import com.github.wz2cool.canal.utils.converter.IValuePlaceholderConverter;
 import com.github.wz2cool.canal.utils.converter.mysql.MysqlAlterSqlConverter;
-import com.github.wz2cool.canal.utils.converter.mysql.MysqlEnhancedAlterSqlConverter;
+import com.github.wz2cool.canal.utils.converter.mysql.MysqlAlterSqlConverterDecorator;
 import com.github.wz2cool.canal.utils.converter.mysql.MysqlValuePlaceholderConverter;
 import com.github.wz2cool.canal.utils.helper.DateHelper;
 import com.github.wz2cool.canal.utils.model.DatabaseDriverType;
@@ -30,7 +30,7 @@ public class MysqlSqlTemplateGenerator extends AbstractSqlTemplateGenerator {
      * @param useEnhancedConverter 如果为true，使用EnhancedMysqlAlterSqlConverter，否则使用默认的MysqlAlterSqlConverter
      */
     public MysqlSqlTemplateGenerator(boolean useEnhancedConverter) {
-        this.alterSqlConverter = useEnhancedConverter ? new MysqlEnhancedAlterSqlConverter() : new MysqlAlterSqlConverter();
+        this.alterSqlConverter = useEnhancedConverter ? new MysqlAlterSqlConverterDecorator(new MysqlAlterSqlConverter()) : new MysqlAlterSqlConverter();
         this.timestampConverter = (mysqlDataType, value) -> DateHelper.getDatetime(value);
     }
 
--- src/test/java/com/github/wz2cool/canal/utils/converter/mysql/MysqlAlterSqlConverterDecoratorTest.java
+++ src/test/java/com/github/wz2cool/canal/utils/converter/mysql/MysqlAlterSqlConverterDecoratorTest.java
@@ -10,14 +10,14 @@ import static org.junit.Assert.assertEquals;
 /**
  * @author penghai
  */
-public class MysqlEnhancedAlterSqlConverterTest {
+public class MysqlAlterSqlConverterDecoratorTest {
 
-    private final MysqlAlterSqlConverter mysqlEnhancedAlterSqlConverter = new MysqlEnhancedAlterSqlConverter();
+    private final MysqlAlterSqlConverterDecorator mysqlAlterSqlConverter =  new MysqlAlterSqlConverterDecorator(new MysqlAlterSqlConverter());
 
     @org.junit.Test
     public void testRenameColumn() throws JSQLParserException {
         String testSql = "ALTER TABLE `qa_penghai`.`bond_amount_2` CHANGE COLUMN `publish_date` `publish_date_2` date NULL DEFAULT NULL COMMENT '公告日期'";
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
         String expectSql = "ALTER TABLE `qa_penghai`.`bond_amount_2` CHANGE COLUMN `publish_date` `publish_date_2` date NULL DEFAULT NULL" +
                 " COMMENT '公告日期';";
         assertEquals(expectSql, result.get(0));
@@ -27,7 +27,7 @@ public class MysqlEnhancedAlterSqlConverterTest {
     public void testChangeColumn() throws JSQLParserException {
         String testSql = "ALTER TABLE `student` CHANGE COLUMN `Column 3` `Column 3` INT(11) NULL DEFAULT NULL COMMENT '公告日期'";
 
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
         assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 3` `Column 3` INT (11) NULL DEFAULT NULL COMMENT '公告日期';", result.get(0));
     }
 
@@ -36,7 +36,7 @@ public class MysqlEnhancedAlterSqlConverterTest {
         String testSql = "ALTER TABLE `student`\n" +
                 "\tDROP COLUMN `Column 4`;\n";
 
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
         assertEquals("ALTER TABLE `student` DROP COLUMN `Column 4`;", result.get(0));
     }
 
@@ -46,10 +46,10 @@ public class MysqlEnhancedAlterSqlConverterTest {
                 "\tCHANGE COLUMN `Column 5` `Column 1` INT(11) UNSIGNED NULL DEFAULT NULL AFTER `name`,\n" +
                 "\tCHANGE COLUMN `Column 4` `Column 2` INT(11) UNSIGNED NULL DEFAULT NULL AFTER `Column 1`;\n";
 
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
 
-        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 5` `Column 1` INT (11) UNSIGNED NULL DEFAULT NULL;", result.get(0));
-        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 2` INT (11) UNSIGNED NULL DEFAULT NULL;", result.get(1));
+        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 5` `Column 1` INT (11) UNSIGNED NULL DEFAULT NULL ;", result.get(0));
+        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 2` INT (11) UNSIGNED NULL DEFAULT NULL ;", result.get(1));
     }
 
     @Test
@@ -58,9 +58,9 @@ public class MysqlEnhancedAlterSqlConverterTest {
                 "\tCHANGE COLUMN `Column 5` `Column 5` VARCHAR(50) NULL DEFAULT NULL AFTER `name`,\n" +
                 "\tCHANGE COLUMN `Column 4` `Column 4` VARCHAR(50) NULL DEFAULT NULL AFTER `Column 5`;\n";
 
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
-        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 5` `Column 5` VARCHAR (50) NULL DEFAULT NULL;", result.get(0));
-        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 4` VARCHAR (50) NULL DEFAULT NULL;", result.get(1));
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
+        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 5` `Column 5` VARCHAR (50) NULL DEFAULT NULL ;", result.get(0));
+        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 4` VARCHAR (50) NULL DEFAULT NULL ;", result.get(1));
     }
 
     @Test
@@ -68,7 +68,7 @@ public class MysqlEnhancedAlterSqlConverterTest {
         String testSql = "ALTER TABLE `student`\n" +
                 "\tCHANGE COLUMN `Column 4` `Column 4` INT (11) UNSIGNED NULL DEFAULT NULL AFTER `Column 5`;\n";
 
-        List<String> result = mysqlEnhancedAlterSqlConverter.convert(testSql);
-        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 4` INT (11) UNSIGNED NULL DEFAULT NULL;", result.get(0));
+        List<String> result = mysqlAlterSqlConverter.convert(testSql);
+        assertEquals("ALTER TABLE `student` CHANGE COLUMN `Column 4` `Column 4` INT (11) UNSIGNED NULL DEFAULT NULL ;", result.get(0));
     }
 }
